3adba987da3ba5d8743e50043fb9a0a60465a3a4
hssdiv <alexdruyk@ukr.net>
2021-01-03 13:32:25 +0200

    implemented basic dog\auth routes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DogsModule } from './dogs/dogs.module';
+import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm'
 
 @Module({
-  imports: [],
+  imports: [AuthModule, DogsModule, TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: '1',
+    database: 'react_app',
+    autoLoadEntities: true,
+    //synchronize: true,
+  }) ],
   controllers: [AppController],
   providers: [AppService],
 })
